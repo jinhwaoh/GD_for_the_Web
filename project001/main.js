f8a2f2ef0e06d0371b17c62b2335b65157b2d3bf
jinhwaoh <jinhwa.j.oh@gmail.com>
2017-04-06 12:37:24 -0400

    change

--- project001/main.js
+++ project001/main.js
@@ -4,13 +4,21 @@ $(document).ready(function() {
   $(".dropdown").click(function(){
   	var text = $("dd",this);
   	var icon = $(this).children('.toggle');
-    
-    text.slideToggle(400, function (){       
+
+    text.slideToggle(400, function (){     
       console.log("dropdown opened");
+
     });
 
     $(this).toggleClass("selected");
+
   });
+
+
+
+
 
 });
 console.log(8+8);
+
+
---
+++ project2/ex2/dia.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+  $(".dropdown").prop("selectedIndex", -1);
+});
